dcc3899ae76f314c2ca28423fe2a84e37d86ae61
Luna <hexishexomgcute@gmail.com>
2025-08-06 03:26:48 +0000

    Odin

--- main.go
+++ main.go
@@ -11,6 +11,37 @@ const (
 
 )
 
+package main
+
+import (
+    "log"
+
+    "github.com/andr1ww/odin"
+)
+
+type User struct {
+    odin.Bucket `bucket:"users" database:"main"`
+    Name        string `json:"name"`
+    Email       string `json:"email"`
+}
+
+func main() {
+    if err := odin.ConnectDefault("./odin.db"); err != nil {
+        log.Fatal(err)
+    }
+    defer odin.CloseAll()
+
+    user := &User{
+        Bucket: odin.Bucket{ID: "Key"},
+        Name:   "Luna",
+        Email:  "Luna@Forge.com",
+    }
+
+    if err := odin.Create(user); err != nil {
+        log.Fatal(err)
+    }
+}
+
 func Routes(r *gin.Engine ) {
 	fmt.Println("Forge Backend created by Luna")
 }
