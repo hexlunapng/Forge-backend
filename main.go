59dc354b537c25bc85440f85fa738b0fd39cc9b0
Luna <hexishexomgcute@gmail.com>
2025-08-06 03:22:53 +0000

    basic main.go file :D

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/andr1ww/odin"
+	"log"
+)
+
+const (
+	port = ":3551"
+
+)
+
+func Routes(r *gin.Engine ) {
+	fmt.Println("Forge Backend created by Luna")
+}
